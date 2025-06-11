96546da421f3636ea78bff088e7099a7c4fddb95
LCurcuma <zhihnadya2002@gmail.com>
2025-06-11 21:32:50 +0200

    added assignment for react1 week2

--- app/destination/page.js
+++ app/destination/page.js
@@ -4,36 +4,36 @@ import { useState } from 'react';
 
 import styles from '@/components/destination/destination.module.css';
 import { AddWishlistItem } from '@/components/destination/AddWishlistItem';
-
-// TASK - React 1 week 2
-// Move this to its own file
-const PlanetWishlistItem = ({
-  name,
-  onRemove,
-  thumbnail,
-}) => {
-  return (
-    <div className={styles.wishlistItem}>
-      <img className={styles.wishlistItemThumbnail} src={thumbnail} alt="" />
-      <b>{name.toUpperCase()}</b>
-      <button onClick={onRemove}>remove</button>
-    </div>
-  );
-}
+import { PlanetWishlistItem } from '@/app/destination/PlanetWishListItem.js';
+import { Footer } from '@/components/ui/Footer.js';
 
 
 export const Destinations = () => {
   const [selectedPlanets, onAddPlanet] = useState([]);
-
-  let isPlanetSelected = false;
-  let numberOfPlanets = 0;
+  const [isPlanetSelected, changeStatusOfPlanet] = useState({status: false, name: ""});
+  const [numberOfPlanets, changeNumberOfPlanets] = useState(0);
 
   const onAddOrRemovePlanet = (name, index) => {
     // TASK - React 1 week 2
     // Implement this function
     // If you press the "ADD PLANET" the selected planet should display "SELECTED"
     // And the counter should update, how many planets are selected (numberOfPlanets)
-    console.log(`You seleceted the following planet: ${name}, with the index of ${index}`);
+
+    //done
+    if(!selectedPlanets.includes(name)){
+      onAddPlanet([...selectedPlanets, name]);
+      changeStatusOfPlanet({status: true, name: name});
+      changeNumberOfPlanets(numberOfPlanets + 1);
+    } 
+    else {
+      changeStatusOfPlanet({status: false, name: ""});
+      onAddPlanet(selectedPlanets.filter(planet => planet !== name));
+      changeNumberOfPlanets(numberOfPlanets - 1);
+    }
+
+    console.log(`You selected the following planet: ${name}, with the index of ${index}`);
+
+    
   }
 
   return (
@@ -45,8 +45,11 @@ export const Destinations = () => {
           {/* TASK - React 1 week 2 */}
           {/* Display the number Of selected planets */}
           {/* Display the "no planets" message if it is empty! */}
-          <p>No planets in wishlist :(</p>
-          <p>You have {numberOfPlanets} in your wishlist</p>
+          {/*Done*/}
+          {selectedPlanets.length === 0 ? (
+            <p>No planets in wishlist :(</p>
+          ) : (
+            <p>You have {numberOfPlanets} planets in your wishlist</p>)}
           <b>List coming soon after lesson 3!</b>
           
           {/* STOP! - this is for week 3!*/}
@@ -79,34 +82,34 @@ export const Destinations = () => {
           {/* Use the README.md file for descriptions */}
           {/* Create a <PlanetCard /> component, which accepts the following properties: */}
           {/* name, description, thumbnail, isSelected, onAddOrRemovePlanet */}
-          <div className={styles.planetCard}>
-            <img className={styles.planetThumbnail} src="/destination/image-europa.png" alt="" />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetSelected ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button 
-              className="roundButton" 
-              onClick={() => onAddOrRemovePlanet('Pluto', 0)}
-            > 
-              {isPlanetSelected ? "REMOVE" : "ADD PLANET"}
-            </button>
-          </div>
-          <div className={styles.planetCard}>
-            <img className={styles.planetThumbnail} src="/destination/image-europa.png" alt="" />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetSelected ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button 
-              className="roundButton" 
-              onClick={() => onAddOrRemovePlanet('Pluto', 0)}
-            > 
-              {isPlanetSelected ? "REMOVE" : "ADD PLANET"}
-            </button>
-          </div>
+
+          {/*done*/}
+
+          <PlanetCard name="Europa" description={"Europa, one of Jupiter's moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers."} thumbnail="/destination/image-europa.png" isSelected={isPlanetSelected} onAddOrRemovePlanet={onAddOrRemovePlanet} />
+          <PlanetCard name="Moon" description={"Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers."} thumbnail="/destination/image-moon.png" isSelected={isPlanetSelected} onAddOrRemovePlanet={onAddOrRemovePlanet} />
+          <PlanetCard name="Mars" description={"Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity's next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth."} thumbnail="/destination/image-mars.png" isSelected={isPlanetSelected} onAddOrRemovePlanet={onAddOrRemovePlanet} />
+          <PlanetCard name="Titan" description={"Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets."} thumbnail="/destination/image-titan.png" isSelected={isPlanetSelected} onAddOrRemovePlanet={onAddOrRemovePlanet} />
         </section>
       </main>
+      <Footer />
+    </div>
+  );
+}
+
+function PlanetCard({ name, description, thumbnail, isSelected, onAddOrRemovePlanet }) {
+  return (
+    <div className={styles.planetCard}>
+      <img className={styles.planetThumbnail} src={thumbnail} alt="" />
+      <div className={styles.planetDescription}>
+        <h2>{name.toUpperCase()} {isSelected.status && isSelected.name === name ? "SELECTED" : ""}</h2>
+        <p>{description}</p>
+      </div>
+      <button 
+        className="roundButton" 
+        onClick={() => onAddOrRemovePlanet(name)}
+      > 
+        {isSelected ? "REMOVE" : "ADD PLANET"}
+      </button>
     </div>
   );
 }
--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -2,6 +2,8 @@
 
 import { usePathname } from 'next/navigation';
 import styles from './Footer.module.css';
+import Link from 'next/link';
+
 
 export const Footer = () => {
   const path = usePathname().split('?')[0];
@@ -15,39 +17,43 @@ export const Footer = () => {
       {/* TASK - React 1 week 2 */}
       {/* Create a new List for the Pages */}
       {/* We need to use the <Link /> component here */}
-      {/* <div className={styles.pages}>
+      
+      {/*done*/}
+      <div className={styles.pages}>
         <h3>Pages</h3>
         <ul>
-          <li> <Link/> </li>
-          ...
+          <li> <Link href="/about_us"> About Us</Link></li>
+          <li> <Link href="/destination"> Destination</Link></li>
+          <li> <Link href="/nasa_collaboration"> NASA Collaboration</Link></li>
         </ul>
-      </div> */}
+      </div>
       {/* Docs for the Link: https://nextjs.org/docs/pages/api-reference/components/link */}
+
+      
 
       {/* TASK - React 1 week 1 */}
       {/* Add a new list item for LINKEDIN */}
       <div className={styles.footerLinks}>
         <h3>Follow us</h3>
-        <ul className={styles.footerList}>
-          <li>
-            <a href="https://facebook.com">Facebook</a>
-          </li>
-          <li>
-            <a href="https://instagram.com">Instagram</a>
-          </li>
-          <li>
-            <a href="https://tiktok.com">Tiktok</a>
-          </li>
-          <li>
-            <a href="https://google.com">On the streets at night</a>
-          </li>
           {/* TASK - React 1 week 2 */}
           {/* Create a <SocialMediaItem /> component and replace all of the list items! */}
           {/* it should accept the following props */}
           {/* url, title, icon */}
           {/* For the icons, you can download 1-2 social media icons for testing and put it in the /public/socialmedia/ folder */}
-        </ul>
+          {/*done*/}
+    <SocialMediaItem url='www.facebook.com' title='Facebook' icon='facebook.jpg'/>
+    <SocialMediaItem url='www.instagram.com' title='Instagram' icon='instagram.jpg'/>
+          <SocialMediaItem url='www.tiktok.com' title='Tiktok' icon='tiktok.jpg'/>
+          <SocialMediaItem url='www.google.com' title='Google' icon='google.png'/>
       </div>
     </footer>
+  );
+}
+
+function SocialMediaItem({ url, title, icon }) {
+  return (
+      <a href={url}>
+        <img src={`/socialmedia/${icon}`} alt={title} className={styles.logo}/>
+      </a>
   );
 }
